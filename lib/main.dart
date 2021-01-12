3d3aa955238636fe27f83a8511896001d90cd3ff
nowxel <nowxel@gmail.com>
2021-01-12 15:55:38 +0200

    Fifth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,17 @@
 void main() {
   print('Hello');
+  print('Hello1');
+  print('Hello2');
+  print('Hello3');
+
+  // First first = First();
+  // first.test();
+
+
+}
+
+class First{
+  void test() {
+    print("hello");
+  }
 }
